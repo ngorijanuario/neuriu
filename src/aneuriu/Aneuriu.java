efdd2b7349aa72c28495206e8da47bd2456aae86
Ngori Januário <68328526+ngorijanuario@users.noreply.github.com>
2020-12-28 07:03:45 +0100

    Add files via upload

---
+++ src/aneuriu/Aneuriu.java
@@ -0,0 +1,15 @@
+
+package aneuriu;
+
+/**
+ *
+ * @author Ngorymilson
+ */
+public class Aneuriu {
+
+    public static void main(String[] args) {
+        janela a = new janela();
+        a.setVisible(true);
+    }
+    
+}
